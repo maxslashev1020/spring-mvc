c0e800a03fd0090c0d60937776474b870c2a96b0
maxslashev <maxslahev1020@gmail.com>
2021-08-18 09:31:53 +0400

    123

--- 2.2.2. spring_mvc/src/main/java/web/dao/Car.java
+++ 2.2.2. spring_mvc/src/main/java/web/dao/Car.java
@@ -1,10 +1,22 @@
-package web.controller;
+package web.dao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Car {
     private String model;
     private String color;
     private Integer price;
 
+    public List<Car> getList() {
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("Audi", "RED", 10000));
+        list.add(new Car("BMW", "YELLOW", 12000));
+        list.add(new Car("Mersedes", "BLACK", 15000));
+        list.add(new Car("Opel", "GREEN", 8000));
+        list.add(new Car("Volkswagen", "BLUE", 5000));
+        return list;
+    }
     public String getModel() {
         return model;
     }
@@ -27,6 +39,9 @@ public class Car {
 
     public void setPrice(Integer price) {
         this.price = price;
+    }
+
+    public Car() {
     }
 
     public Car(String model, String color, Integer price) {
--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -1,11 +1,14 @@
-package web.controller;
+package web.service;
+
+import web.dao.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarService {
+    Car car;
     public static List<Car> getCarsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
+        if (number <= 0 || number > 5) return list;
         return list.stream().limit(number).collect(Collectors.toList());
     }
 }
